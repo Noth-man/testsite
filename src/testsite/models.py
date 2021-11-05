7ffd5b7c3d42b06ad3e40ae0076d8a834878a6d4
Noth-man <memillfountain@gmail.com>
2021-11-05 11:23:25 +0900

    model

--- src/testsite/models.py
+++ src/testsite/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Threads(models.Model):
+    id = models.IntegerField()
+    name = models.CharField(max_length=100)
+
+class Comments(models.Model):
+    id = models.IntegerField()
+    thread_id = models.ForeignKey(Threads, on_delete=models.CASCADE)
+    paremt_id = models.IntegerField()
+    body = models.TextField()
+    pub_date = models.DateTimeField('date published')

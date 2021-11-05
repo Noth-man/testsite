abfe2f6d9fd199e831a73ee352b2845d9b880dee
Noth-man <memillfountain@gmail.com>
2021-11-05 12:47:02 +0900

    model

--- src/testsite/admin.py
+++ src/testsite/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Threads, Comments
 # Register your models here.
+
+admin.site.register(Threads)
+admin.site.register(Comments)
--- src/testsite/models.py
+++ src/testsite/models.py
@@ -3,12 +3,16 @@ from django.db import models
 # Create your models here.
 
 class Threads(models.Model):
-    id = models.IntegerField()
+    id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=100)
+    class Meta:
+       verbose_name_plural = "スレッド"
 
 class Comments(models.Model):
-    id = models.IntegerField()
+    id = models.BigAutoField(primary_key=True)
     thread_id = models.ForeignKey(Threads, on_delete=models.CASCADE)
     paremt_id = models.IntegerField()
     body = models.TextField()
     pub_date = models.DateTimeField('date published')
+    class Meta:
+       verbose_name_plural = "コメント"

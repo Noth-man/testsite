7a4cd47e8ec894e5a8929954b2db2c485f9b97c5
Noth-man <memillfountain@gmail.com>
2021-11-05 17:15:55 +0900

    createThread

--- src/testsite/urls.py
+++ src/testsite/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
- 
+
+app_name = 'testsite'
+
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('createThread/', views.CreateThreadView.as_view(), name='createThread'),
 ]
--- src/testsite/views.py
+++ src/testsite/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import Threads, Comments
 # Create your views here.
 
 class IndexView(TemplateView):
     template_name = 'testsite/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['thread_list'] = Threads.objects.all()
+        context['comment_list'] = Comments.objects.all()
+        return context
+
+class CreateThreadView(TemplateView):
+    template_name = 'testsite/createThread.html'
